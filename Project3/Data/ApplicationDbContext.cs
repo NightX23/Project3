f84b3f8ff79313c429b9d272f4487f6a89568296
NightX23 <marlon231098@hotmail.com>
2018-12-04 13:49:54 -0400

    Update
    Addition and configuration of the "ServiceType"

--- Project3/Data/ApplicationDbContext.cs
+++ Project3/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Project3.Models;
 
 namespace Project3.Data
 {
@@ -12,5 +13,7 @@ namespace Project3.Data
             : base(options)
         {
         }
+
+        public DbSet<ServiceType> ServiceTypes { get; set; }
     }
 }
